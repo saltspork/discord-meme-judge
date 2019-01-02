e7e3c1fe358243aa13c58c9de5f0a32c3b87da50
saltspork <saltspork@gmail.com>
2019-01-02 15:11:37 +1030

    changed to rewritten discord.py library

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ python3 -m venv venvdir
 source venvdir/bin/activate.fish
 
 pip3 install wheel
-pip3 install -U https://github.com/Rapptz/discord.py/archive/async.zip#egg=discord.py
+pip3 install -U https://github.com/Rapptz/discord.py/archive/rewrite.zip#egg=discord.py
 pip3 install -r requirements.txt
 
 ./meme-judge.py
--- meme-judge.py
+++ meme-judge.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python3
 
-import discord, asyncio, aiohttp, datetime, io, json, collections, math, datetime
+import discord, asyncio, aiohttp, datetime, io, json, collections, math, datetime, traceback
 
 with open('config.json') as f:
 	config = json.load(f, object_pairs_hook=collections.OrderedDict)
 
+# Channels that memes are sorted into, in which only messages requesting edits are allowed
 nospam = []
 for channel in config['channels']:
 	nospam.extend(list(config['channels'][channel]['reacts'].values()))
 
 processing = []
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('Invite: ' + discord.utils.oauth_url(client.user.id))
-	print('------\n')
+class MyClient(discord.Client):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		# create the background task and run it in the background
+		self.bg_task = self.loop.create_task(self.refresh_memes())
+
+	async def on_ready(self):
+		print('Logged in as')
+		print(self.user.name)
+		print(self.user.id)
+		print('------')
+
+	async def refresh_memes(self):
+		logtime('main refresh loop entered')
+		await self.wait_until_ready()
+		logtime('main refresh loop ready')
+		while True:
+			if not self.is_closed():
+				logtime('global refresh starting')
+				for channel in config['channels']:
+					async for logmsg in self.get_channel(int(channel)).history():
+						await evaluate_meme(logmsg)
+				logtime('global refresh finished, sleeping for '+str(config['refresh_interval']))
+			else:
+				logtime('global refresh skipped, connection not open')
+			await asyncio.sleep(config['refresh_interval'])
+		logtime('main refresh loop exited')
+
+client = MyClient()
 
 @client.event
 async def on_message(message):
@@ -29,16 +52,6 @@ async def on_message(message):
 async def on_reaction_add(reaction, user):
 	await evaluate_meme(reaction.message)
 
-async def refresh_memes():
-	await client.wait_until_ready()
-	while not client.is_closed:
-		logtime('global refresh starting')
-		for channel in config['channels']:
-			async for logmsg in client.logs_from(client.get_channel(channel)):
-				await evaluate_meme(logmsg)
-		logtime('global refresh finished, sleeping for '+str(config['refresh_interval']))
-		await asyncio.sleep(config['refresh_interval'])
-	logtime('global refresh loop exited, this is probably bad')
 
 def lookup_emoji(prefix, server):
 	for emoji in server.emojis:
@@ -58,13 +71,17 @@ def matchreact(react, valids):
 # lazy wrapper to avoid races
 async def evaluate_meme(message):
 	if message.id in processing:
-		logtime(message.id+' race averted')
+		logtime(str(message.id)+' race averted')
 		return
 	else:
 		processing.append(message.id)
-		logtime(message.id+' evaluation starting')
-		await unsafe_evaluate_meme(message)
-		logtime(message.id+' evaluation finished')
+		logtime(str(message.id)+' evaluation starting')
+		try:
+			await unsafe_evaluate_meme(message)
+		except Exception as e:
+			print("PYTHON ERROR: " + str(e))
+			print(traceback.format_exc())
+		logtime(str(message.id)+' evaluation finished')
 		processing.remove(message.id)
 
 async def unsafe_evaluate_meme(message):
@@ -72,62 +89,59 @@ async def unsafe_evaluate_meme(message):
 		return
 	
 	if message.channel.id in nospam:
-		logtime(message.id+' editing possible')
-		if message.content.startswith("<@"+client.user.id+"> "):
+		logtime(str(message.id)+' editing possible')
+		if message.content.startswith("<@"+str(client.user.id)+"> "):
 			tokens=message.content.split(' ', 2)
-			editme = await client.get_message(message.channel, tokens[1])
+			editme = await message.channel.get_message(tokens[1])
 			if editme.author.id == client.user.id:
-				await client.edit_message(editme, editme.content + '\n' + tokens[2] + ' (' + message.author.mention + ')')
-				logtime(message.id+' editing edited')
-		await client.delete_message(message)
-		logtime(message.id+' editing deleted')
+				await editme.edit(content=editme.content + '\n' + tokens[2] + ' (' + message.author.mention + ')')
+				logtime(str(message.id)+' editing edited')
+		await message.delete()
+		logtime(str(message.id)+' editing deleted')
 		return
 	
-	if message.channel.id not in config['channels']:
+	if str(message.channel.id) not in config['channels']:
 		return
 
-	if 'whitelist' in config['channels'][message.channel.id]:
-		if message.id in config['channels'][message.channel.id]['whitelist']:
+	if 'whitelist' in config['channels'][str(message.channel.id)]:
+		if message.id in config['channels'][str(message.channel.id)]['whitelist']:
 			return
 
 	if len(message.attachments) == 0 and 'http' not in message.content.lower():
-		logtime(message.id+' invalid deleting')
-		await client.delete_message(message)
-		logtime(message.id+' invalid deleted')
+		logtime(str(message.id)+' invalid deleting')
+		await message.delete()
+		logtime(str(message.id)+' invalid deleted')
 		return
 
 	users = {client.user.id:[]}
 	valid_mess = []
 	invalid_mess = []
 
-	logtime(message.id+' valid enumerating')
+	logtime(str(message.id)+' valid enumerating')
 
 	for reaction in message.reactions:
-		for user in await client.get_reaction_users(reaction):
+		async for user in reaction.users():
 			if user.id in users:
 				users[user.id].append(str(reaction.emoji))
 			else:
 				users[user.id] = [str(reaction.emoji)]
 
-	logtime(message.id+' valid enumerated')
+	logtime(str(message.id)+' valid enumerated')
 
-	if len(message.attachments) > 0 and message.attachments[0]['size'] >= config['max_size']:
-		if str(config['alert']) not in users[client.user.id]:
-			logtime(message.id+' oversize placeholding')
-			await client.add_reaction(message, config['alert'])
-			logtime(message.id+' oversize placeholded')
-		logtime(message.id+' oversize skipping')
-		return
+	if len(message.attachments) > 0 and message.attachments[0].size >= config['max_size']:
+		warntxt = message.author.mention + ' your meme ' + message.attachments[0].filename + ' is larger than ' + str(config['max_size']/1000) + ' KB.\nPlease compress it more until bots get Nitro.'
+		await client.get_channel(config['channels'][str(message.channel.id)]['infochan']).send(warntxt)
+		await message.delete()
 
-	for reaction in config['channels'][message.channel.id]['reacts']:
+	for reaction in config['channels'][str(message.channel.id)]['reacts']:
 		if reaction.startswith('<:'):
-			reactwith = lookup_emoji(reaction, message.server)
+			reactwith = lookup_emoji(reaction, message.guild)
 		else:
 			reactwith = reaction
 		if str(reactwith) not in users[client.user.id] and reactwith != False:
-				logtime(message.id+' valid placeholding '+str(reactwith))
-				await client.add_reaction(message, reactwith)
-				logtime(message.id+' valid placeholded '+str(reactwith))
+				logtime(str(message.id)+' valid placeholding '+str(reactwith))
+				await message.add_reaction(reactwith)
+				logtime(str(message.id)+' valid placeholded '+str(reactwith))
 
 	del users[client.user.id]
 
@@ -136,7 +150,7 @@ async def unsafe_evaluate_meme(message):
 		invalid_counted = []
 
 		for react in users[user]:
-			valid, subreact = matchreact(react, config['channels'][message.channel.id]['reacts'])
+			valid, subreact = matchreact(react, config['channels'][str(message.channel.id)]['reacts'])
 			if not valid:
 				invalid_counted.append(subreact)
 			elif subreact not in valid_counted:
@@ -158,57 +172,58 @@ async def unsafe_evaluate_meme(message):
 	if margin < 1:
 		return True
 
-	if config['channels'][message.channel.id]['reacts'][valid_grouped[0][0]] == 'delete' and margin < 2:
+	if config['channels'][str(message.channel.id)]['reacts'][valid_grouped[0][0]] == 'delete' and margin < 2:
 		return True
 
 	# after target seconds, only a margin of 1 is required
-	elapsed = (datetime.datetime.utcnow() - message.timestamp).total_seconds()
+	elapsed = (datetime.datetime.utcnow() - message.created_at).total_seconds()
 
-	if margin < math.ceil(1-math.log(elapsed/config['channels'][message.channel.id]['target'], 2)) and not config['immediate']:
+	if margin < math.ceil(1-math.log(elapsed/config['channels'][str(message.channel.id)]['target'], 2)) and not config['immediate']:
 		return True
 
 	return await sentence_meme(message, valid_grouped + invalid_grouped)
 
 
 async def sentence_meme(message, reacts):
-	logtime(message.id+' valid voted')
-	if config['channels'][message.channel.id]['reacts'][reacts[0][0]] == 'delete':
-		await client.delete_message(message)
-		logtime(message.id+' valid deleted')
+	logtime(str(message.id)+' valid voted')
+	if config['channels'][str(message.channel.id)]['reacts'][reacts[0][0]] == 'delete':
+		await message.delete()
+		logtime(str(message.id)+' valid deleted')
 		return
-	elif config['channels'][message.channel.id]['reacts'][reacts[0][0]] == 'stalemate':
+	elif config['channels'][str(message.channel.id)]['reacts'][reacts[0][0]] == 'stalemate':
 		return True
 
-	target = client.get_channel(config['channels'][message.channel.id]['reacts'][reacts[0][0]])
+	target = client.get_channel(config['channels'][str(message.channel.id)]['reacts'][reacts[0][0]])
 	memetxt = message.author.mention + '  |  '
 	for reaction in reacts:
 		reactwith = reaction[0]
 		if reactwith.startswith('<:') and not reactwith.endswith('>'):
-			reactwith = str(lookup_emoji(reactwith, message.server))
+			reactwith = str(lookup_emoji(reactwith, message.guild))
 		memetxt += reactwith + ' ' + str(reaction[1]) + '  |  '
 	if message.content:
 		memetxt += '\n' + message.content
 
 	if len(message.attachments) > 0:
-		logtime(message.id+' valid attachment downloading')
+		logtime(str(message.id)+' valid attachment downloading')
 		async with aiohttp.ClientSession() as session:
-			async with session.get(message.attachments[0]['url']) as response:
+			async with session.get(message.attachments[0].url) as response:
 				attached_bytes = await response.read()
 				attachment = io.BytesIO(attached_bytes)
-		logtime(message.id+' valid attachment uploading')
-		await client.send_file(target, attachment, filename=message.attachments[0]['filename'] ,content=memetxt)
-		logtime(message.id+' valid attachment uploaded')
+		logtime(str(message.id)+' valid attachment uploading')
+		await target.send(memetxt, file=discord.File(attachment, message.attachments[0].filename))
+		logtime(str(message.id)+' valid attachment uploaded')
 	else:
-		logtime(message.id+' valid textonly posting')
-		await client.send_message(target, memetxt)
-		logtime(message.id+' valid textonly posted')
+		logtime(str(message.id)+' valid textonly posting')
+		await target.send(memetxt)
+		logtime(str(message.id)+' valid textonly posted')
 
-	await client.delete_message(message)
-	logtime(message.id+' valid original deleted')
+	await message.delete()
+	logtime(str(message.id)+' valid original deleted')
 
 def logtime(message):
 	print(str(datetime.datetime.now())+' '+message)
 
 
-client.loop.create_task(refresh_memes())
 client.run(config['token'])
+
+
